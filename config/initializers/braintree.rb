9c833261f58d22f449e9db9fe4660511c9d38b87
Glenn Goodrich <glenn.goodrich@gmail.com>
2014-09-14 16:25:48 -0400

    Fix typo

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment  = ENV['BRAINTREE_ENV']         || :sandbox
-Braintree::Configuratio.merchant_id   = ENV['BRAINTREE_MERCHANT_ID'] || '4svnrzjwkqch6p9r'
+Braintree::Configuration.merchant_id  = ENV['BRAINTREE_MERCHANT_ID'] || '4svnrzjwkqch6p9r'
 Braintree::Configuration.public_key   = ENV['BRAINTREE_PUBLIC_KEY']  || 'hcgxdwtsx2hkxn4b'
 Braintree::Configuration.private_key  = ENV['BRAINTREE_PRIVATE_KEY'] || '6592e601c729c6f801e9ce5e24d63d14'
