406d01255d478e01136a3bb76e8529accf101beb
Glenn Goodrich <glenn.goodrich@gmail.com>
2014-09-15 18:20:29 -0400

    Payments being accepted

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,15 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @params = params
-    render action: :new
+    nonce = params[:payment_method_nonce]
+    render action: :new and return unless nonce
+    result = Braintree::Transaction.sale(
+      amount: "10.00",
+      payment_method_nonce: nonce
+    )
+
+    flash[:notice] = "Sale successful. Head to Sizzler" if result.success?
+    flash[:alert] = "Something is amiss. #{result.transaction.processor_response_text}" unless result.success?
+    redirect_to action: :new
   end
 end
