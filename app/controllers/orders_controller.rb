20a067e3c1334bfdfe49efb6d0c3b45e322d270b
Glenn Goodrich <glenn.goodrich@gmail.com>
2014-09-14 20:20:35 -0400

    Payment form in place, rendering params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,9 @@ class OrdersController < ApplicationController
   def new
     @client_token = Braintree::ClientToken.generate
   end
+
+  def create
+    @params = params
+    render action: :new
+  end
 end
